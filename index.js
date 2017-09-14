6c3bda0e0ffb1cbfcb4857b17be43e106d14242b
gewisser <roman@gavrilow.ru>
2017-09-14 19:10:25 +0300

    clear index

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-/**
- * Created by Roman on 31.08.2017.
- */
 
-
-import MAutocomplete from './components/matcss_autocomplete.vue'
-import MCheckBox from './components/matcss_checkbox.vue'
-import MCollapsible from './components/matcss_collapsible.vue'
-import MCollections from './components/matcss_collections.vue';
-import MDropdown from './components/matcss_dropdownButton.vue';
-import MInput from './components/matcss_input.vue';
-import MList from './components/matcss_list.vue';
-import MModal from './components/matcss_modal.vue';
-import MSelect from './components/matcss_select.vue';
-
-export {MAutocomplete, MCheckBox, MCollapsible, MCollections, MDropdown, MInput, MList, MModal, MSelect};
