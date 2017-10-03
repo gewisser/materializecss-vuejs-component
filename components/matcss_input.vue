43b809b8a40ef2b439fb5588fd06a7f6b582a7ff
gewisser <roman@gavrilow.ru>
2017-10-03 13:14:06 +0300

    Added ext prop.
    Add prop 'disabled' [bollean type or 1-disabled, 0-enabled]
    Add prop 'validation' [you cat write: validation="val != ''"]. this is prop javascript code as string. If return 'false', then added class 'invalid', else 'valid' to input element.
    Add prop 'add-class'. This is mixins to exist class. can be object or string. Example: '{'center-align': true}'
    Add prop 'data-error' and 'data-success'. This is  custom validation messages.
    Add prop 'is-valid'. -1: unknown, 0: invalid, 1: valid. This is prop sync.
    Add prop 'icon-prefix'. Specify the name of the icon before the field;

--- components/matcss_input.vue
+++ components/matcss_input.vue
@@ -4,40 +4,105 @@ matcss_input.vue
 
 <template lang="pug">
     .input-field
-        input.validate(:id="GUIDID", :value="val", @change="onChange", :type="type==undefined?'text': type", :class="{ valid: textExist }")
-        label(:for='GUIDID', :class="{ active: textExist}") {{ name }}
+        i.material-icons.prefix(v-if="iconPrefix !== undefined") {{ iconPrefix }}
+        input(
+            :id="GUIDID",
+            :value="val",
+            @change="onChange",
+            :type="type==undefined?'text': type",
+            :class="c_inputClass",
+            :disabled="c_disabled"
+        )
+        label(:for='GUIDID', :class="{ active: textExist}", :data-error="dataError", :data-success="dataSuccess") {{ name }}
 </template>
 
 <script>
     export default {
-        props: ['name', 'val', 'type'],
+        props: ['name', 'val', 'type', 'disabled', 'validation', 'isValid', 'dataError', 'dataSuccess', 'iconPrefix', 'addClass'],
         name: 'matcss_input',
         data () {
             return {
-                GUIDID: undefined
-            }
-        },
-        created(){
-            this.GUIDID = Materialize.guid();
-        },
-        methods: {
-            onChange() {
-                this.$emit('update:val', $(this.$el).children('input').val());
+                GUIDID: undefined,
+                inputClass: {
+                    valid: false,
+                    invalid: false
+                },
+
             }
         },
         computed: {
             textExist(){
                 return this.val !== undefined && this.val !== '';
+            },
+            c_inputClass(){
+                let addClass;
+
+                if (typeof this.addClass === 'object')
+                    addClass = this.addClass;
+                else
+                    addClass = new Function('', 'return '+this.addClass)();
+
+                return Object.assign({}, this.inputClass, addClass);
+            },
+            c_disabled(){
+                if (this.disabled == undefined) return false;
+
+                if (typeof this.disabled === 'boolean')
+                    return this.disabled
+                else
+                    return this.disabled == 1? true: false;
+            }
+        },
+        created(){
+            this.GUIDID = Materialize.guid();
+        },
+        watch:{
+            isValid(val){
+                switch (val) {
+                    case -1:
+                        this.inputClass.valid = false;
+                        this.inputClass.invalid = false;
+
+                        break;
+                    case 0:
+                        this.inputClass.valid = false;
+                        this.inputClass.invalid = true;
+
+                        break;
+                    case 1:
+                        this.inputClass.valid = true;
+                        this.inputClass.invalid = false;
+
+                        break;
+                }
+            }
+        },
+        methods: {
+            onChange() {
+                this.UpdateVal($(this.$el).children('input').val());
+            },
+
+            UpdateVal(val){
+                if (this.validation != undefined) {
+                    let run = new Function('val', 'return '+this.validation);
+                    let result = run(val);
+
+                    //this.isValid(result);
+
+                    this.$emit('update:isValid', result? 1:0);
+                }
+
+                this.$emit('update:val', val);
             }
         },
         mounted(){
             const _this = this;
 
-            $(this.$el).children('input').keypress(function (e) {
+            let input = $(this.$el).children('input').keypress(function (e) {
                 var val = $(this).val();
 
                 if (e.which == 13 && val && val != '') {
-                    _this.$emit('update:val', val);
+                    _this.UpdateVal(val);
 
                     return true;
                 }
@@ -47,6 +112,3 @@ matcss_input.vue
         }
     }
 </script>
-
-<style>
-</style>
