3e33f5062f95c97e4cdcd4db0c045fb944562a60
gewisser <roman@gavrilow.ru>
2017-10-18 19:24:38 +0300

    A new component has been added - progress

---
+++ components/matcss_progress.vue
@@ -0,0 +1,68 @@
+//
+Created by Roman on 13.10.2017.
+matcss_progress.vue
+
+<template lang="pug">
+    .row
+        .col(:class="divrows", style="padding-right: 3px;")
+            .progress
+                div(:style='st', :class='mode_calc')
+        .col.s4(v-if="isLabel") {{ percent }}%
+</template>
+
+<script>
+
+    export default {
+        props: ['val', 'min', 'max', 'mode', 'label'],
+        name: 'matcss_progress',
+        data() {
+            return {
+                percent: '',
+                divrows: 's12'
+            }
+        },
+        computed: {
+            st(){
+                let d = this.max - this.min;
+                let percent = (this.val * 100)/d
+
+                this.percent = Math.round((percent)*10)/10;
+
+                return {
+                    width: percent+'%'
+                }
+            },
+            mode_calc(){
+                let mode = 'determinate';
+
+                if (this.mode != undefined)
+                    mode = this.mode;
+
+                return mode;
+            },
+            isLabel(){
+                let isLabel = this.label != undefined && this.label == 1? true: false;
+
+                this.divrows = isLabel? 's8': 's12';
+
+                return isLabel
+            }
+        },
+        created(){
+            const _this = this;
+            bus.$on('matcss_progress', function (v) {
+                let run = new Function('ts', v);
+                run(_this);
+            })
+        }
+    }
+</script>
+<style scoped>
+    .row .col {
+        padding: 0 0;
+    }
+    .progress{
+        padding-top: 7px;
+        margin-top: 0.55rem;
+    }
+</style>
---
+++ matcss_progress.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Roman on 14.09.2017.
+ */
+
+import MProgress from './components/matcss_progress.vue';
+export {MProgress as default};
