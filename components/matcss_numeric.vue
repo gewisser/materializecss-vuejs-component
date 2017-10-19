2a0e2cdc60f27b3f8159aea8ed210cdde0f54842
gewisser <roman@gavrilow.ru>
2017-10-19 18:39:03 +0300

    The new "numeric" component.
    This component allows you to enter numeric values, as well as increase or decrease values using the buttons to the left and right of the input field.

---
+++ components/matcss_numeric.vue
@@ -0,0 +1,69 @@
+//
+Created by Roman on 19.10.2017.
+matcss_numeric.vue
+
+<template lang="pug">
+    span
+        a.btn.btn-floating.waves-effect.waves-light(style="margin-left: 12px;", @click="OnDecrement")
+            i.material-icons remove
+        m-input-internal.inline(
+        :style="inputStyle",
+        :name="name",
+        :val="c_val",
+        :disabled="disabled",
+        :validation="validation",
+        :is-valid="isValid",
+        :data-error="dataError",
+        :data-success="dataSuccess",
+        :add-class="addClass",
+        :placeholder="placeholder",
+        numeric="true"
+        )
+        a.btn.btn-floating.waves-effect.waves-light(style="margin-left: 5px;", @click="OnIncrement")
+            i.material-icons add
+</template>
+
+<script>
+    import MInput from './matcss_input.vue';
+
+    export default {
+        props: [
+            'name',
+            'val',
+            'disabled',
+            'validation',
+            'isValid',
+            'dataError',
+            'dataSuccess',
+            'addClass',
+            'placeholder',
+            'inputStyle'
+        ],
+        name: 'matcss_numeric',
+        data() {
+            return {
+            }
+        },
+        computed:{
+            c_val(){
+                return this.val == undefined || this.val == ''? '0': this.val
+            }
+        },
+        components: {
+            'm-input-internal': MInput
+        },
+        methods:{
+            OnDecrement(){
+                let val = parseInt(this.c_val) - 1;
+                this.$emit('update:val', val);
+            },
+            OnIncrement(){
+                let val = parseInt(this.c_val) + 1;
+                this.$emit('update:val', val);
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ matcss_numeric.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Roman on 14.09.2017.
+ */
+
+import MNumeric from './components/matcss_numeric.vue';
+export {MNumeric as default};
