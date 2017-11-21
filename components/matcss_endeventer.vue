fbcebd7bb04d95f0cc70b1d44c5d46ec6a98b998
gewisser <roman@gavrilow.ru>
2017-11-21 15:14:41 +0300

    Add new component: endeventer.

---
+++ components/matcss_endeventer.vue
@@ -0,0 +1,28 @@
+//
+Created by Roman on 18.11.2017.
+matcss_endeventer.vue
+
+<template lang="pug">
+    .scrollspy(style="height: 10px")
+</template>
+
+<script>
+    export default {
+        name: 'matcss_endeventer',
+        mounted(){
+            this.$nextTick(function () {
+                const sspy = $(this.$el);
+                const _this = this;
+                sspy.scrollSpy({
+                    scrollOffset: 0,
+                    activeClass: 'active',
+                    getActiveElement(id){
+                        if (!sspy.hasClass('active'))
+                            _this.$emit('onScroll');
+                        return sspy;
+                    }
+                });
+            })
+        }
+    }
+</script>
---
+++ matcss_endeventer.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Roman on 14.09.2017.
+ */
+
+import MEndEventer from './components/matcss_endeventer.vue';
+export {MEndEventer as default};
