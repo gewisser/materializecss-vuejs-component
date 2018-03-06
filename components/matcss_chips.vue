8df3a46675420dcd987a04f318d0c7d99e889af9
gewisser <roman@gavrilow.ru>
2018-03-06 13:57:00 +0300

    Added new component "Chips"

---
+++ components/matcss_chips.vue
@@ -0,0 +1,56 @@
+//
+Created by Roman on 01.03.2018.
+matcss_chips.vue
+
+<template lang="pug">
+    div
+        label {{ name }}
+        .chips
+</template>
+
+<script>
+    export default {
+        props: ['val', 'name', 'placeholder', 'secondaryPlaceholder'],
+        name: 'matcss_chips',
+        created(){
+            this.selfChange = false; // Initiate here, since the reactivity of the variable is not needed
+        },
+        watch: {
+            val(val){
+                if (!this.selfChange) // detect self change. If not detect and val props use as [ :val.sync="..." ], then the parameters are reset and the focus of the component is lost.
+                    this.setData(this.val);
+
+                this.selfChange = false;
+            }
+        },
+        mounted () {
+            const _this = this;
+            this.$nextTick(function () {
+                this.chp = $(this.$el).find('.chips');
+
+                this.setData(this.val);
+
+                this.chp.on('chip.add', function(e, chip){
+                    _this.OnUpdate();
+                });
+
+                this.chp.on('chip.delete', function(e, chip){
+                    _this.OnUpdate();
+                });
+            })
+        },
+        methods:{
+            setData(val){
+                this.chp.material_chip({
+                    placeholder: this.placeholder,
+                    secondaryPlaceholder: this.secondaryPlaceholder,
+                    data: this.val
+                });
+            },
+            OnUpdate(){
+                this.selfChange = true;
+                this.$emit('update:val', this.chp.material_chip('data'));
+            }
+        }
+    }
+</script>
---
+++ matcss_chips.js
@@ -0,0 +1,2 @@
+import MChips from './components/matcss_chips.vue'
+export {MChips as default};
